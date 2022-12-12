66d1ce307b47cf46b67b1bd5944b2eb14b576b38
Akshwin <akshwint.2003@gmal.com>
2022-12-12 19:12:24 +0530

    added sum of natural numbers

---
+++ 10_Iterative_sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int sum(int n)
+{
+   int sum = 0;
+   for ( int i = 1 ; i <=n ;i++)
+   {
+    sum = sum+i;
+   }
+   return sum;
+
+}
+int main()
+{
+    printf("%d",sum(4));
+    return 0;
+}
