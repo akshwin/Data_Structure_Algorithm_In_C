d4f633e17c21e1d757e59e012abbb6c451cd8a63
Akshwin <akshwint.2003@gmal.com>
2022-12-18 19:30:03 +0530

    added

--- 3_Array_ADT/2_Inserting.c
+++ 3_Array_ADT/2_Inserting.c
@@ -1,7 +1,26 @@
 #include<stdio.h>
 
+struct Array 
+{
+    int A[20];
+    int size;
+    int length;
+};
+
+
+void Display(struct Array arr)
+{
+    for ( int i = 0 ; i < arr.length ; i ++)
+    {
+        printf("%d",arr.A[i]);
+    }
+}
+
 int main()
 {
+    struct Array arr = {{1,2,3,4,5,6},10,6};
+
+    Display(arr);
     
 
     return 0;
