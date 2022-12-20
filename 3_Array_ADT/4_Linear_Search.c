5b87ba61f0746ad45a02d7d0657b41327aa8915c
Akshwin <akshwint.2003@gmal.com>
2022-12-20 19:32:07 +0530

    added

--- 3_Array_ADT/4_Linear_Search.c
+++ 3_Array_ADT/4_Linear_Search.c
@@ -1,8 +1,38 @@
 #include<stdio.h>
 
+struct Array 
+{
+    int A[10];
+    int size;
+    int length;
+};
+
+void Display(struct Array arr)
+{
+    for( int  i = 0 ; i < arr.length ; i++)
+    {
+        printf("%d",arr.A[i]);
+    }
+}
+
+int LSearch(struct Array arr , int key)
+{
+    for( int i = 0 ; i < arr.length ; i++)
+    {
+        if(key == arr.A[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+
+}
+
 int main()
 {
+    struct Array arr = {{1,2,3,4,5},10,5};
+    int x = LSearch(arr,4);
+    printf("%d",x);
     
-
     return 0;
 }
