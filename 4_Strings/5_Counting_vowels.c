f71b4b4b1176a64420aca428d40581b6221c0b3d
Akshwin <akshwint.2003@gmail.com>
2022-12-30 23:38:37 +0530

    added

--- 4_Strings/5_Counting_vowels.c
+++ 4_Strings/5_Counting_vowels.c
@@ -7,6 +7,7 @@ int main()
     char A[] = "Welcome to home";
     for ( i = 0 ; A[i]!='\0';i++)
     {
+        if(A[i] == 'a' ||A[i] == 'e'||A[i] == 'i'||A[i] == 'o'||A[i] == 'u'||A[i] == 'A' || A[i] == 'E'||A[i] == 'I'||A[i] == 'O'|| A[i] == 'U')
         count ++;
     }
     printf("%d",count);
