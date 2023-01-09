9e7c39bc4998d739dae2a96999c31eceea858a8e
Akshwin <akshwint.2003@gmail.com>
2023-01-09 21:49:59 +0530

    added

--- 5_Linked_List/7_Insert.c
+++ 5_Linked_List/7_Insert.c
@@ -16,7 +16,7 @@ void create(int A[] , int n)
     first->next = NULL;
     last = first;
 
-    for(i = 0 ; i < n ; i++)
+    for(i = 1 ; i < n ; i++)
     {
         t = (struct Node *)malloc(sizeof(struct Node));
         t->data = A[i];
@@ -26,13 +26,63 @@ void create(int A[] , int n)
     }
 }
 
+int Count(struct Node *p)
+{
+    int l = 0;
+    while(p!=NULL)
+    {
+        l++;
+        p = p->next;
+    }
+    return l;
+}
+
+void Insert(struct Node *p , int index , int x)
+{
+    struct Node *t;
+    if(index < 0 ||  index >Count(p))
+        return ;
+    t = (struct Node *)malloc(sizeof(struct Node));
+    t->data = x;
+    
+
+    if(index == 0 )
+    {
+        t->next = first;
+        first = t;
+    }
+    else
+    {
+        for( int i = 0 ; i<index -1 ;i++)
+            p=p->next;
+        t->next = p->next;
+        p->next = t;
+    }
+}
+
+void Display(struct Node *p)
+{
+    while(p!=NULL)
+    {
+        printf("%d ",p->data);
+        p = p->next;
+    }
+
+}
+
 int main()
 {
     struct Node *temp;
-    int A[] = {3,5,7,10,15};
+    int A[] = {3,5,7};
 
-    create(A,5);
+    create(A,3);
+    Display(first);
+    printf("\n");
+    Insert(first , 0, 10);
+    Display(first);
+    printf("\n");
+    Insert(first , 4, 15);
+    Display(first);
    
-
     return 0;
 }
